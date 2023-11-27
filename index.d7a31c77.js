28a549fe06d81142fbcbce1e111ce14fd1a67c4a
FlowMaster83 <110739720+FlowMaster83@users.noreply.github.com>
2023-11-27 17:36:07 +0000

    Deploying to gh-pages from @ FlowMaster83/JS-npm@fe3490df2e9afab860c1e763d54db05adab4edec 🚀

---
+++ index.d7a31c77.js
@@ -0,0 +1,2 @@
+const o=2+3;console.log(o);console.log(2*1);
+//# sourceMappingURL=index.d7a31c77.js.map
---
+++ index.e671bccb.js
@@ -0,0 +1,2 @@
+!function(){var o=2+3;console.log(o);console.log(2*1)}();
+//# sourceMappingURL=index.e671bccb.js.map
