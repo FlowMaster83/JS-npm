4d10b2f26996209efdb2c2d8fac9ae9a57fec999
FlowMaster83 <110739720+FlowMaster83@users.noreply.github.com>
2023-12-01 18:01:42 +0000

    Deploying to gh-pages from @ FlowMaster83/JS-npm@9f65ad2202ef63f4fdaf41ce43ec41058a7b4134 🚀

---
+++ index.26b335a5.js
@@ -0,0 +1,2 @@
+!function(){setTimeout((function(){console.log("Прошло >= 3 секунд после автосейва")}),3e3);var o=new Date;console.log(o.getMonth());console.log(["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"][o.getMonth()]);console.log(["Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun"][o.getDay()])}();
+//# sourceMappingURL=index.26b335a5.js.map
